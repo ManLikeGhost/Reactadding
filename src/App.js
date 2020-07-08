ffdd1622cbadffb3572f96311d8e349b4954161c
ManLikeGhost <ekpoabasiama@gmail.com>
2020-07-08 16:46:15 +0100

    done with this

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component }from 'react';
 import './App.css';
+import Sales from './Sales';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    var list = [
+      {name: 'Ruby', price: 200},
+      {name: 'Python', price: 100},
+      {name: 'Java', price: 150},
+      {name: 'Kotlin', price: 250},
+    ]
+    return (
+      <div className="App">
+        <Sales items={list}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/Sales.js
+++ src/Sales.js
@@ -1,13 +1,65 @@
 import React, { Component } from 'react'
 
 class Sales extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             total: 0
+        }
+
+        this.addPrice = this.addPrice.bind(this);
+    }
+
+    addPrice(price){
+        this.setState({total:this.state.total + price});
+    }
+
     render() {
+        console.log(this.props.items);
+        var langs = this.props.items.map((item, i) => {
+            return <Lang name={item.name} price={item.price}
+                key={i} addPrice={this.addPrice} active={item.active}
+                />
+        })
         return (
             <div>
-                
+                <h1>You can buy languages:</h1>
+                <div id="courses">
+                    {langs}
+                    <p id="total">Total: <b>{this.state.total}</b></p>
+                </div>
             </div>
         )
     }
 }
+
+
+class Lang extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             active: false
+        };
+
+        this.clicker = this.clicker.bind(this);
+    }
+
+    clicker(){
+        var active =!this.state.active
+        this.setState({active: active});
+        this.props.addPrice(active ? this.props.price : -this.props.price)
+    }
+
+    render() {
+        return (
+            <div>
+               <p className={this.state.active ? 'active' : ''} onClick={this.clicker}>{this.props.name} <b>{this.props.price}</b></p> 
+            </div>
+        )
+    }
+}
+
 
 export default Sales
